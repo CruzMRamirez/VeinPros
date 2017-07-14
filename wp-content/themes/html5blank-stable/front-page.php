62ebba137adc5e9b6a893a80c59bf32c1a8f271d
KuanaxBon <cruz_ramirez@ymail.com>
2017-07-13 23:34:47 -0700

    edited front-page added a few graphics

--- wp-content/themes/html5blank-stable/front-page.php
+++ wp-content/themes/html5blank-stable/front-page.php
@@ -5,26 +5,55 @@
                 <h1>Welcome to VeinPros</h1>
             </div>
         </div>
-        <div class="row content justify-content-around">
+        <div class="row content justify-content-around" style="margin: 40px;">
             <div class="col-md-5 col-sm-12">
+                <h2>Hello World</h2>
+                <hr>
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt sagittis diam eu maximus. Integer ultricies, turpis sit amet mollis tincidunt, felis lacus pretium neque, in aliquet risus justo a risus. Mauris eu luctus arcu. Etiam ut urna tincidunt, aliquet augue vitae, accumsan enim. Quisque venenatis non tortor ut ornare. Nunc ac arcu massa. Morbi convallis rhoncus ex vitae vestibulum. Vivamus ac eros aliquam, eleifend erat a, scelerisque massa. Morbi eu ultricies felis. Mauris fermentum cursus mi ut molestie. Cras condimentum quis libero ac imperdiet. Nunc feugiat posuere nulla, tristique tincidunt tellus tempus vitae. Aliquam aliquet laoreet nisl. Aenean imperdiet sagittis sem non ullamcorper. Suspendisse volutpat eros vitae aliquam pretium. Integer scelerisque imperdiet fermentum.</p>
                 <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur lobortis orci libero, vitae semper diam eleifend porttitor. Maecenas non turpis vehicula, dignissim arcu nec, pellentesque nisl. Donec eleifend sodales leo id gravida. Donec ex arcu, porttitor a vehicula ut, rhoncus ac justo. Praesent a ex nisi. Donec aliquam vulputate finibus. Duis eleifend velit et lectus ornare euismod in vitae odio. Proin vehicula accumsan viverra. Proin in faucibus justo. Curabitur lacinia laoreet laoreet. Morbi finibus ac augue quis scelerisque. Nam sit amet sem auctor, pretium nulla eget, ultrices lectus. Praesent vel porttitor velit.</p>
             </div>
             <div class="col-md-5 col-sm-12">
+                <h2>Hello World</h2>
+                <hr>
                 <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur lobortis orci libero, vitae semper diam eleifend porttitor. Maecenas non turpis vehicula, dignissim arcu nec, pellentesque nisl. Donec eleifend sodales leo id gravida. Donec ex arcu, porttitor a vehicula ut, rhoncus ac justo. Praesent a ex nisi. Donec aliquam vulputate finibus. Duis eleifend velit et lectus ornare euismod in vitae odio. Proin vehicula accumsan viverra. Proin in faucibus justo. Curabitur lacinia laoreet laoreet. Morbi finibus ac augue quis scelerisque. Nam sit amet sem auctor, pretium nulla eget, ultrices lectus. Praesent vel porttitor velit.</p>
                 <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur lobortis orci libero, vitae semper diam eleifend porttitor. Maecenas non turpis vehicula, dignissim arcu nec, pellentesque nisl. Donec eleifend sodales leo id gravida. Donec ex arcu, porttitor a vehicula ut, rhoncus ac justo. Praesent a ex nisi. Donec aliquam vulputate finibus. Duis eleifend velit et lectus ornare euismod in vitae odio. Proin vehicula accumsan viverra. Proin in faucibus justo. Curabitur lacinia laoreet laoreet. Morbi finibus ac augue quis scelerisque. Nam sit amet sem auctor, pretium nulla eget, ultrices lectus. Praesent vel porttitor velit.</p>
             </div>
         </div>
-        <div class="row videoRow justify-content-between colorize">
+        <div class="row videoRow hidden-sm-down colorize">
             <div class="col-8">
-                <div id="bigVideo"></div>
+                <div class="bigVideo"></div>
             </div>
-            <div class="col-4" style="background: #fff; padding:0;">
-                <div class="col-12 colorize">
+            <div class="col-4">
+                <div class="col-md-12 col-sm-6 colorize" style="margin-bottom: 2vw;">
                     <div class="smallVideo"></div>
                 </div>
-                <div class="col-12 colorize">
+                <div class="col-md-12 col-sm-6 colorize" style="margin-top: 1vw;">
                     <div class="smallVideo"></div>
+                </div>
+            </div>
+        </div>
+        <div class="row videoRow hidden-md-up colorize">
+            <div class="col-12">
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner" role="listbox">
+                            <div class="carousel-item active">
+                            <div class="bigVideo bigvideomobile"></div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="bigVideo bigvideomobile"></div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="bigVideo bigvideomobile"></div>
+                            </div>
+                        </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
             </div>
         </div>
