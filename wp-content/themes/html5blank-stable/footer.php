7a827e492cd0935b7343749eb2960f45ef034805
Crescent <roqueacenteno.rac@gmail.com>
2017-07-13 23:34:41 -0700

    added a gradient

--- wp-content/themes/html5blank-stable/footer.php
+++ wp-content/themes/html5blank-stable/footer.php
@@ -1,12 +1,43 @@
 			<!-- footer -->
 			<footer class="footer" role="contentinfo">
 
-				<div class="my_stuff col-md-5 text-center"> Using Bootsrap is Cool </div>
+				<div class="style-bottom">
+				<!--3 divs of info content below-->
+					<div class="row">
+						<div class="col-md-4">
+							<p class="bold-font mbottom">Lorem</p>
+							<ul>
+								<li>Lorem Ipsum</li>
+								<li>Lorem Ipsum</li>
+								<li>Lorem Ipsum</li>
+								<li>Lorem Ipsum Dolor</li>
+								<li>Lorem</li>
+								<li>Lorem</li>
+							</ul>
+						</div>
+						<div class="col-md-4">
+							<p class="bold-font mbottom">Lorem Ipsum</p>
+							<ul>
+								<li>Lorem Ipsum</li>
+								<li>Lorem Ipsum Dolor Sit</li>
+								<li>Lorem Ipsum</li>
+								<li>Lorem</li>
+								<li>Lorem</li>
+								<li>Lorem Ipsum Dolor</li>
+							</ul>
+						</div>
+						<div class="col-md-4">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						</div>
+					</div>
 				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
-				</p>
+				<div class="text-center">
+					<p class="copyright">
+						&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
+						<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
+					</p>
+				</div>
+			</div>
 				<!-- /copyright -->
 
 			</footer>
