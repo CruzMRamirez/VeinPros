ea2893cc27ae42686b917f27acdb582e6e045fb3
KuanaxBon <cruz_ramirez@ymail.com>
2017-07-18 00:33:16 -0700

    messed with the navigation

--- wp-content/themes/html5blank-stable/header.php
+++ wp-content/themes/html5blank-stable/header.php
@@ -21,30 +21,54 @@
             tests: {}
         });
         </script>
-
+		<?php $page = "Home"?>
 	</head>
 	<body <?php body_class(); ?>>
-
 		<!-- wrapper -->
 		<div class="pagewrapper">
 
 			<!-- header -->
 			<header class="header clear" role="banner">
-				<nav class="row justify-content-end">
-					<div class="col">
-						<a href="#"><img src="wp-content/uploads/2017/07/editedLogo.png" height="110" width="170" alt="Logo"></a>
+				<nav class="navbar navbar-toggleable-md navbar-light row justify-content-between">
+					<a class="img" href="#"><img src="/wp-content/uploads/2017/07/editedLogo.png" height="110" width="170" alt="Logo"></a>
+					<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav2" aria-controls="navbarNav2" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse align-self-end" id="navbarNav2">
+						<div class="hidden-lg-up row justify-content-center">
+							<div class="col">
+								<?php
+									wp_nav_menu( array(
+									'theme_location' => 'VeinPro-Main-Menu',
+									'container_class' => 'custom-menu-class' ) );
+								?>
+							</div>
+						</div>
 					</div>
-					<div class="hidden-sm-down col-7 row justify-content-end align-self-end">
-						<button class="active">Home</button>
-						<button>Symptoms-Veins</button>
-						<button>Treatment</button>
-						<button>Testimonials</button>
-						<button>My Blog</button>
-						<button>Contact-Us</button>
+					<div class="collapse navbar-collapse align-self-end" id="navbarNav">
+						<?php
+							wp_nav_menu( array(
+							'theme_location' => 'VeinPro-Main-Menu',
+							'container_class' => 'custom-menu-class' ) );
+						?>
+					</div>
+				</nav>
+
+			</header>
+			<!-- /header -->
+<!-- 			<nav class="row justify-content-end">
+					<div class="col">
+						<a class="img" href="#"><img src="/wp-content/uploads/2017/07/editedLogo.png" height="110" width="170" alt="Logo"></a>
+					</div>
+					<div class="links hidden-sm-down col-7 row justify-content-end align-self-end">
+						<a <php if ($page == "Home"){ echo "class='active'";} ?>>Home</a>
+						<a <php if ($page == "Symptoms-Veins"){ echo "class='active'";} ?>>Symptoms-Veins</a>
+						<a <php if ($page == "Treatment"){ echo "class='active'";} ?>>Treatment</a>
+						<a <php if ($page == "Testimonials"){ echo "class='active'";} ?>>Testimonials</a>
+						<a <php if ($page == "Blog"){ echo "class='active'";} ?>>My Blog</a>
+						<a <php if ($page == "Contact-Us"){ echo "class='active'";} ?>>Contact-Us</a>
 					</div>
 					<div class="hidden-md-up col row justify-content-end align-self-end">
 						<button ><img src="http://www.iconninja.com/files/327/943/625/more-home-menu-hamburger-icon.png" height="80" width="80"alt=""></button>	
 					</div> 
-				</nav>
-			</header>
-			<!-- /header -->
+				</nav>-->
