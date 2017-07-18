2c5bcf7a693e9dface5690ba40a9c24c8dc69802
KuanaxBon <cruz_ramirez@ymail.com>
2017-07-18 05:29:12 -0700

    hotfix for header nav logo

--- wp-content/themes/html5blank-stable/header.php
+++ wp-content/themes/html5blank-stable/header.php
@@ -31,9 +31,9 @@
 			<header class="header clear" role="banner">
 				<nav class="navbar navbar-toggleable-md navbar-light row justify-content-between">
 					<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav2" aria-controls="navbarNav2" aria-expanded="false" aria-label="Toggle navigation">
-					<a class="img" href="#"><img src="/wp-content/uploads/2017/07/editedLogo.png" height="110" width="170" alt="Logo"></a>
 						<span class="navbar-toggler-icon"></span>
 					</button>
+					<a class="img" href="#"><img src="/wp-content/uploads/2017/07/editedLogo.png" height="110" width="170" alt="Logo"></a>
 					<div class="collapse navbar-collapse align-self-end" id="navbarNav2">
 						<div class="hidden-lg-up row justify-content-center">
 							<div class="col">
