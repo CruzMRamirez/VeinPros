332850d286d68e755ec5e249d441b81ae82968b5
KuanaxBon <cruz_ramirez@ymail.com>
2017-07-20 18:25:31 -0700

    hotfix for section content

--- wp-content/themes/html5blank-stable/single.php
+++ wp-content/themes/html5blank-stable/single.php
@@ -1,8 +1,7 @@
 <?php get_header(); ?>
 	<main role="main">
 		<!-- section -->
-		<section class="content">
-            <section class="veincontent">
+		<section class="veincontent">
 			<?php $bgImage = get_the_post_thumbnail_url(); ?>
 			<div class="jumbotron row align-items-center justify-content-center" style="background-image:url(<?php echo $bgImage; ?>);">
 					<div class="col-12 black_highlight text-center">
