6d6fd1126821ce961c39908818afbea8145db373
KuanaxBon <cruz_ramirez@ymail.com>
2017-07-23 23:44:00 -0700

    edited nav to stay on certain sizes

--- wp-content/themes/html5blank-stable/front-page.php
+++ wp-content/themes/html5blank-stable/front-page.php
@@ -33,6 +33,7 @@
         <div class="row videoRow hidden-sm-down colorize">
             <div class="col-8">
                 <div class="bigVideo">
+                    <video class="homepage--video hidden-sm-down" src=""></video>
                 </div>
             </div>
             <div class="col-4">
